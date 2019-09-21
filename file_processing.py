6586e670674814b0ba998117f6f730c94cf7d96d
Sakchhi Srivastava <sakchhi.sri@gmail.com>
2019-09-21 23:58:25 +0530

    Correct broken command-line tool

--- file_processing.py
+++ file_processing.py
@@ -10,7 +10,7 @@ def read_file(file_name):
     # print(inspect.stack()[1][1].split('/')[-1])
     if inspect.stack()[1][1].split('/')[-1] == 'directory_processing.py':
         write_content.write_to_directory(file_name, content)
-    elif inspect.stack()[1][1].split('/')[-1] == 'file_processing.py':
+    elif inspect.stack()[0][1].split('/')[-1] == 'file_processing.py':
         write_content.write_to_file(file_name, content)
     elif inspect.stack()[2][1].split('/')[-1] == 'flask_api.py':
         print("Inside the conditional")
